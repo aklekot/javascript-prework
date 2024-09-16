1e2e5cffe815d125c3bbd0e740eb040517ec4ada
Agnieszka Klekot <74aklekot@gmail.com>
2024-09-16 10:28:45 +0200

    adding if

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@ var playerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
-printMessage('Zagrałem' + playerMove + 'Wygrałem');
+printMessage('Zagrałem ' + playerMove + ! 'Wygrałem!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2')  {
+  computerMove = 'papier';
+}
+else {
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
